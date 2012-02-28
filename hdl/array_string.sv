9f9c525d6f397f0de22767e0b1174a391ab436cf
Iztok Jeras <iztokj@aptina.com>
2012-02-28 16:24:26 +0100

    added some more string methods

--- hdl/array_string.sv
+++ hdl/array_string.sv
@@ -1,13 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// tests related to the 'string' type                                         //
+//                                                                            //
+// In ncsim there seem to be issued with getting a single character from a    //
+// string, it seems the standard specifies the use of the first character but //
+// ncsim is using the rightmost one.                                          //
+// Another issue are substrings, the same sintax as for the part of an array  //
+// test_string[i:j] is not legal in ncsim.                                    //
+// In ncsim the string method realtoa() seems to fail parsing the exponent    //
+// notation.                                                                  //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
 module array_string #(
   // parameters for array sizes
   parameter WS = 8  // string length
 )();
 
+// local parameters
 localparam string ARRAY_S0 = "Janez Novak";
 
 // ASCII text strings
 string string_s1;
 string string_s2 = "Hello, world!";
+
+// numbers
+integer i;
+real    r;
 
 // arrays
 bit   [0:WS-1][7:0] array_bit;
@@ -20,7 +39,6 @@ initial begin
 end
 
 task test_string_array;
-  integer i;
 begin
   // assigning a value to a string and display it
   string_s1 = "Test of strings as arrays.";
@@ -46,16 +64,26 @@ begin
   // replication
   string_s1 = {3{string_s2, " "}};
   $write ("test string is: \"%s\"\n", string_s1);
-  // index putc
+  // indexed write character
   string_s1 = string_s2;
   string_s1[12] = "?.";
   $write ("test string is: \"%s\"\n", string_s1);
   string_s1 = string_s2;
   string_s1[12] = 16'h3f2e;
   $write ("test string is: \"%s\"\n", string_s1);
-  // index getc
+  // indexed read character
   $write ("test string is: \"%s\" \"%s\"\n", ARRAY_S0[0], ARRAY_S0[10]);
   $write ("test string is: \"%s\" \"%s\"\n", string_s2[0], string_s2[12]);
+//  // indexed write substring
+//  string_s1 = string_s2;
+//  string_s1[11:12] = "?.";
+//  $write ("test string is: \"%s\"\n", string_s1);
+//  string_s1 = string_s2;
+//  string_s1[11:12] = 16'h3f2e;
+//  $write ("test string is: \"%s\"\n", string_s1);
+//  // indexed read substring
+//  $write ("test string is: \"%s\" \"%s\"\n", ARRAY_S0[0:1]);
+//  $write ("test string is: \"%s\" \"%s\"\n", string_s2[0:1]);
 end
 endtask
 
@@ -92,6 +120,34 @@ begin
   $write ("compare:    %0d\n", ARRAY_S0.compare("Janez Novak"));
   $write ("compare:    %0d\n", ARRAY_S0.compare("janez novak"));
   $write ("icompare:   %0d\n", ARRAY_S0.icompare("janez novak"));
+  // substring
+  $write ("compare:    \"%s\"\n", ARRAY_S0.substr(0, 4));
+  // string to integer
+  string_s1 = "356";
+  $write ("atoi:       'd%s = %0d\n", string_s1, string_s1.atoi());
+  string_s1 = "DEAD_BEEF";
+  $write ("atohex:     'h%s = %0d\n", string_s1, string_s1.atohex());
+  string_s1 = "356";
+  $write ("atooct:     'o%s = %0d\n", string_s1, string_s1.atooct());
+  string_s1 = "10100101";
+  $write ("atobin:     'b%s = %0d\n", string_s1, string_s1.atobin());
+  string_s1 = "0.033";
+  $write ("atoreal:    %s = %0e\n", string_s1, string_s1.atoreal());
+  string_s1 = "3.300000e-02";
+  $write ("atoreal:    %s = %0f\n", string_s1, string_s1.atoreal());
+  // integer to string
+  i = 356;
+  string_s1.itoa(i);
+  $write ("itoa:       %0d = 'd%s\n", i, string_s1);
+  string_s1.hextoa(i);
+  $write ("hextoa:     %0d = 'd%s\n", i, string_s1);
+  string_s1.octtoa(i);
+  $write ("itoa:       %0d = 'd%s\n", i, string_s1);
+  string_s1.bintoa(i);
+  $write ("itoa:       %0d = 'd%s\n", i, string_s1);
+  r = 0.033;
+  string_s1.realtoa(r);
+  $write ("realtoa:    %0f = 'd%s\n", r, string_s1);
 end
 endtask
 
