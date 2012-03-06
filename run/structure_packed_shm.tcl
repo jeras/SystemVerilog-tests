5b48d2e3fccd1f284ef7cdaaf3da3d5b916e4896
Iztok Jeras <iztokj@aptina.com>
2012-03-06 13:08:28 +0100

    test structure_packed_wave is working now

--- run/structure_packed_shm.tcl
+++
@@ -1,3 +0,0 @@
-database -open waves -shm -into wav
-probe -create -database waves structure_packed_wave_tb -all -depth all -memories -tasks
-run
--- run/structure_packed_vcd.tcl
+++
@@ -1,3 +0,0 @@
-database -open waves -vcd -into wav.vcd
-probe -create -database waves structure_packed_wave_tb -all -depth all -memories -tasks
-run
