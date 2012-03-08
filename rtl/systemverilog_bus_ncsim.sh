c05b549cdc1f29f498319e7798d1bc80d3ba0eb1
Iztok Jeras <iztokj@aptina.com>
2012-03-08 18:00:08 +0100

    added option to run the netlist

--- rtl/systemverilog_bus_ncsim.sh
+++ rtl/systemverilog_bus_ncsim.sh
@@ -1,6 +1,14 @@
+#!/bin/sh
+
 irun -64bit -access +r -input systemverilog_bus_ncsim.tcl \
 systemverilog_bus_def.sv \
 systemverilog_bus_tb.sv \
 systemverilog_bus_wrap.sv \
 systemverilog_bus_mux.sv \
 systemverilog_bus_demux.sv
+
+irun -64bit -access +r -input systemverilog_bus_ncsim.tcl \
+systemverilog_bus_tb.sv \
+altera_primitives.v \
+stratixiv_atoms.v \
+-v stratix4/stratix4.vqm
