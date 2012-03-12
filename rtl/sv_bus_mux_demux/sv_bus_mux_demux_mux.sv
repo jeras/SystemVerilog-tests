7be343343adbcb317013ff68fb6b0b4670572459
Iztok Jeras <iztokj@aptina.com>
2012-03-12 21:00:30 +0100

    added some comments

--- rtl/sv_bus_mux_demux/sv_bus_mux_demux_mux.sv
+++ rtl/sv_bus_mux_demux/sv_bus_mux_demux_mux.sv
@@ -10,52 +10,62 @@ import package_str::*;
 
 module sv_bus_mux_demux_mux (
   // system signals
-  input  logic        clk,
-  input  logic        rst,
+  input  logic        clk,      // clock
+  input  logic        rst,      // reset
   // input bus
   input  logic        bus_vld,  // valid (chip select)
   input  logic [31:0] bus_adr,  // address
   input  logic [31:0] bus_dat,  // data
   output logic        bus_rdy,  // ready (acknowledge)
   // output stream
-  output logic        str_vld,
-  output logic  [7:0] str_bus,
-  input  logic        str_rdy
+  output logic        str_vld,  // valid (chip select)
+  output logic  [7:0] str_bus,  // byte data bus
+  input  logic        str_rdy   // ready (acknowledge)
 );
 
-logic       bus_trn;
-logic       str_trn;
+logic       bus_trn;  // bus    data transfer
+logic       str_trn;  // stream data transfer
 
-logic [2:0] pkt_cnt;
-logic       pkt_end;
+logic [2:0] pkt_cnt;  // packet byte counter
+logic       pkt_end;  // packet byte counter end
 
-t_bus       pkt_bus;
-t_str       pkt_str;
+t_bus       pkt_bus;  // transfer packet as a structure
+t_str       pkt_str;  // transfer packet as an array
 
+// bus data transfer
 assign bus_trn = bus_vld & bus_rdy;
 
+// ready if pipe is empty or output is ready
 assign bus_rdy = ~str_vld | pkt_end;
 
+// writing input address/data into a structure
 always @ (posedge clk)
 if (bus_trn) begin
   pkt_bus.adr <= bus_adr;
   pkt_bus.dat <= bus_dat;
 end
 
+// TODO, this should be an union
 assign pkt_str = pkt_bus;
 
+// output valid is set by an input transfer
+// or cleared by the last output transfer
 always @ (posedge clk, posedge rst)
 if (rst)           str_vld <= 1'b0;
 else               str_vld <= bus_trn | (str_vld & ~pkt_end);
 
+// packet byte counter
 always @ (posedge clk, posedge rst)
 if (rst)           pkt_cnt <= 4'd0;
 else if (str_trn)  pkt_cnt <= pkt_cnt + 3'd1;
 
+// packet byte counter end
 assign pkt_end = str_rdy & (&pkt_cnt);
 
+// TODO, this should be a registered signal
 assign str_bus = pkt_str [pkt_cnt];
 
+// stream data transfer
 assign str_trn = str_vld & str_rdy;
 
 endmodule : sv_bus_mux_demux_mux
