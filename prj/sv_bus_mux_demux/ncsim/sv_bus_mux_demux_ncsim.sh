fab33ad9a804d88ed19fedf7043328ca97b5f672
Iztok Jeras <iztokj@aptina.com>
2012-03-15 20:39:49 +0100

    added uption to simulate netlist from synplify_pro

--- prj/sv_bus_mux_demux/ncsim/sv_bus_mux_demux_ncsim.sh
+++ prj/sv_bus_mux_demux/ncsim/sv_bus_mux_demux_ncsim.sh
@@ -7,8 +7,8 @@ irun -64bit -access +r -input sv_bus_mux_demux_ncsim.tcl \
 ../hdl/sv_bus_mux_demux_mux.sv \
 ../hdl/sv_bus_mux_demux_demux.sv
 
-#irun -64bit -access +r -input sv_bus_mux_demux_ncsim.tcl \
-#sv_bus_mux_demux_tb.sv \
-#altera_primitives.v \
-#stratixiv_atoms.v \
-#-v stratix4/stratix4.vqm
+irun -64bit -access +r -input sv_bus_mux_demux_ncsim.tcl \
+../hdl/sv_bus_mux_demux_tb.sv \
+../hdl/altera_primitives.v \
+../hdl/stratixiv_atoms.v \
+-v ../synplify/stratix4/stratix4.vqm
